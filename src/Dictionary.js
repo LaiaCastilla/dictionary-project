e1c56ebb6cf6cabb79742d3a0315f8e9cf088ff2
Aylhenia <lcastillaiamoros@gmail.com>
2023-09-05 16:40:58 +0200

    API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
+
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [searchWord, setSearchWord] = useState("");
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
+    let apiKey = `fdt0a6ab6o2733f48fa51ccaa0c76a01`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${searchWord}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
     alert(`Searching for ${searchWord}`);
   }
   function handleSearchWord(event) {

d6b4f55cdaf1008c97f93d2c00aea2171c99aa0f
Aylhenia <lcastillaiamoros@gmail.com>
2023-09-06 11:17:11 +0200

    Added synonyms and examples

--- src/Meanings.js
+++ src/Meanings.js
@@ -6,13 +6,28 @@ export default function Meanings(props) {
     <div className="Meanings">
       <h3 className="part-of-speech">{props.meanings.partOfSpeech}</h3>
       <p className="definition">Definition: {props.meanings.definition}</p>
-
-      {/* <p className="example">
+      <p className="example">
         <em>
-          {props.meanings.example.charAt(0).toUpperCase() +
-            props.meanings.example.slice(1)}
+          {props.meanings.example !== null
+            ? `"${
+                props.meanings.example.charAt(0).toUpperCase() +
+                props.meanings.example.slice(1)
+              }"`
+            : null}
         </em>
-      </p> */}
+      </p>
+      <p className="synonyms">
+        {props.meanings.synonyms !== null
+          ? props.meanings.synonyms.map(function (synonym, index) {
+              return (
+                <span key={index}>
+                  {index === 0 ? "" : ", "}
+                  {synonym}
+                </span>
+              );
+            })
+          : null}
+      </p>
     </div>
   );
 }

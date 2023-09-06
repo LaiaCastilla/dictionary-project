6f46b7ff4d25f7f335453ffb20da6c07f6531979
Aylhenia <lcastillaiamoros@gmail.com>
2023-09-06 18:24:11 +0200

    added Photos (gave up carousel)

--- src/Photos.js
+++ src/Photos.js
@@ -3,39 +3,27 @@ import React from "react";
 export default function Photos(props) {
   if (props.photos) {
     return (
-      <div className="Photos">
-        <div
-          id="carouselExampleSlidesOnly"
-          class="carousel slide"
-          data-ride="carousel"
-        >
-          <div class="carousel-inner">
-            {props.photos.map(function (photo, index) {
-              return (
-                <section key={index}>
-                  {index === 0 ? (
-                    <div class="carousel-item active">
-                      <img
-                        class="d-block w-100"
-                        src={photo.src.landscape}
-                        alt="First slide"
-                      />
-                    </div>
-                  ) : (
-                    <div class="carousel-item">
-                      <img
-                        class="d-block w-100"
-                        src={photo.src.landscape}
-                        alt="Second slide"
-                      />
-                    </div>
-                  )}
-                </section>
-              );
-            })}
-          </div>
+      <section className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a
+                  href={photo.src.original}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt={photo.photographer}
+                  />
+                </a>
+              </div>
+            );
+          })}
         </div>
-      </div>
+      </section>
     );
   } else {
     return null;

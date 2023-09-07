1459ee2365a2b362d36b521582b46d7e63114baf
Aylhenia <lcastillaiamoros@gmail.com>
2023-09-07 10:26:34 +0200

    trying to fix the mistyped word error

--- src/Results.js
+++ src/Results.js
@@ -4,27 +4,32 @@ import Meanings from "./Meanings";
 import "./Results.css";
 
 export default function Results(props) {
-  if (props.results) {
-    return (
-      <div className="Results">
-        <section>
-          <div className="word-phon">
-            <span className="word">{props.results.word} </span>
-            <span className="phonetics">/{props.results.phonetic}/</span>
-          </div>
-        </section>
-        {props.results.meanings.map(function (meaning, index) {
-          return (
-            <section>
-              <div key={index}>
-                <Meanings meanings={meaning} />
-              </div>
-            </section>
-          );
-        })}
-      </div>
-    );
-  } else {
-    return null;
+  try {
+    if (props.results) {
+      return (
+        <div className="Results">
+          <section>
+            <div className="word-phon">
+              <span className="word">{props.results.word} </span>
+              <span className="phonetics">/{props.results.phonetic}/</span>
+            </div>
+          </section>
+          {props.results.meanings.map(function (meaning, index) {
+            return (
+              <section>
+                <div key={index}>
+                  <Meanings meanings={meaning} />
+                </div>
+              </section>
+            );
+          })}
+        </div>
+      );
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+    return "Try typing a different word";
   }
 }
